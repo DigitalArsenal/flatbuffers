834fe907dd3e191e2db3007b9d3c8b16b1db2df1
tjkoury <tj@digitalarsenal.io>
2025-03-02 14:28:34 -0500

    debugging working

--- tests/MyGame/Example/Ability.py
+++ tests/MyGame/Example/Ability.py
@@ -27,39 +27,3 @@ def CreateAbility(builder, id, distance):
     builder.PrependUint32(distance)
     builder.PrependUint32(id)
     return builder.Offset()
-
-
-class AbilityT(object):
-
-    # AbilityT
-    def __init__(self):
-        self.id = 0  # type: int
-        self.distance = 0  # type: int
-
-    @classmethod
-    def InitFromBuf(cls, buf, pos):
-        ability = Ability()
-        ability.Init(buf, pos)
-        return cls.InitFromObj(ability)
-
-    @classmethod
-    def InitFromPackedBuf(cls, buf, pos=0):
-        n = flatbuffers.encode.Get(flatbuffers.packer.uoffset, buf, pos)
-        return cls.InitFromBuf(buf, pos+n)
-
-    @classmethod
-    def InitFromObj(cls, ability):
-        x = AbilityT()
-        x._UnPack(ability)
-        return x
-
-    # AbilityT
-    def _UnPack(self, ability):
-        if ability is None:
-            return
-        self.id = ability.Id()
-        self.distance = ability.Distance()
-
-    # AbilityT
-    def Pack(self, builder):
-        return CreateAbility(builder, self.id, self.distance)
